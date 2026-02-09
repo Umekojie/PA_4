0c21f9e5fbee31cd28c4192edb7d3e9ad4c6510b
Josephine Cattell <josieohio7967@gmail.com>
2026-02-08 22:28:18 -0500

    add for function

--- PA_4.py
+++ PA_4.py
@@ -2,3 +2,7 @@
 Josephine Cattell
 this program will create a list of 100 random numbers from 1-10 """
 import random
+
+list_of_numbers = []
+for i in range (1,10):
+    list_of_numbers.append(random.randint(1,10))
